a2bd7108039bf29a16659b525a10b05b7c651434
Volodymyr <heravolodymyr1991@gmail.com>
2022-09-15 21:16:42 +0300

    make new project

---
+++ DriveMoto/Mapper/MaperProfile.cs
@@ -0,0 +1,19 @@
+﻿using DriveMoto.Models;
+using System.Security.Claims;
+using AutoMapper;
+
+namespace DriveMoto.Mapper
+{
+    public class MaperProfile : Profile
+    {
+        public MaperProfile()
+        {
+            CreateMap<Client, ClientDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<CartItem, CartItemDTO>()
+                .ForMember(t => t.Product, t => t.MapFrom(x => x.Product))
+                .ForMember(t => t.Client, t => t.MapFrom(x => x.Client))
+                .ReverseMap();
+        }
+    }
+}
